62eaf13f90beca87a2ab63323e18ec8b2afa5eb7
Adam Nelson <anelson@apocryph.org>
2011-08-04 21:07:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awspend::Application.config.secret_token = '93e90d1aaa1c6acb0ecf625d2e93ffaf135cf750f2f917a8c73fa2dd449c2622e967bf8e5dfcb7e07233781e61481d5eb2f1e8a1584a265d271088335c600e0c'
